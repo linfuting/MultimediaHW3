a736ec4be796d712470ed8931c05da82b951866f
林福廷 <nana14352000@gmail.com>
2018-01-30 17:39:54 +0800

    測試讀圖檔

--- HW3.cpp
+++ HW3.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+using namespace cv;
 
 int main()
 {
+	Mat i1 = imread("i1.pgm");
+	namedWindow("i1");
+	imshow("i1", i1);
+	waitKey(0);
     return 0;
 }
 
